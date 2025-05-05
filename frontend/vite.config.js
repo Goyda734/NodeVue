bba0548f61dcc03f168c8f255009db8aa29f228e
Goyda734 <rossov.mark@mail.ru>
2025-05-05 23:43:01 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,22 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'url'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
 
 export default defineConfig({
-  base: '/', // Всегда используем абсолютные пути
   plugins: [vue()],
-  server: {
-    port: 8000,
-    strictPort: true, // Не искать другие порты, если 8000 занят
-  },
+  base: '/', // Важно для корректных путей
   build: {
-    outDir: path.resolve(__dirname, '../backend/public'),
+    outDir: '../backend/public',
     emptyOutDir: true,
-    assetsDir: 'assets', // Явно указываем папку для ассетов
     rollupOptions: {
-      input: path.resolve(__dirname, 'index.html') // Можно упростить
+      input: {
+        main: fileURLToPath(new URL('./index.html', import.meta.url))
+      }
     }
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
