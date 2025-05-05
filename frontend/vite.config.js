d6d4f709785e0b6ad6f1bc6b7c68137feb9c0c08
Goyda734 <rossov.mark@mail.ru>
2025-05-05 23:58:23 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,9 +6,8 @@ export default defineConfig({
   plugins: [vue()],
   base: '/',
   build: {
-    outDir: path.resolve(__dirname, '../backend/public'),
+    outDir: '../backend/public',
     emptyOutDir: true,
-    assetsDir: 'assets',
     rollupOptions: {
       input: path.resolve(__dirname, 'index.html')
     }
